a5621fecbeec03af379e92566f6b86b945389377
Parth <iitkgp.sinha.parth@gmail.com>
2024-01-24 21:00:12 +0530

    Changed the UI of Premium button

--- client/src/components/Workspace.js
+++ client/src/components/Workspace.js
@@ -14,20 +14,20 @@ const Workspace = () => {
 
 
   return (
-    <div className="bg-[#121212] text-white pb-2">
+    <div className="bg-[url('../src/images/background.jpg')] text-white pb-2">
 
       {/* top-arrow */}
      <GoUpArrow />
 
 
       {/* navbar */}
-      <div className="flex justify-between py-2 pl-4 pr-[5%] items-center sticky top-0 bg-[#121212] z-10">
+      <div className="flex justify-between py-2 pl-4 pr-[5%] items-center top-0 z-10">
         <div className="text-center text-cyan-500 text-[2.307rem] font-extrabold font-['Inter']">Slinkly</div>
         <div className="flex justify-center items-center">
-          <button className="mr-10 text-xl p-1.5  rounded-md bg-cyan-500 hidden sm:inline">PREMIUM</button>
+          <button className="mr-10 text-xl p-1.5  rounded-[48px] text-yellow-600 pl-[25px] pr-[25.05px] py-[11px] hidden sm:inline">PREMIUM</button>
           <span onClick={handleUserDropdown} className="flex justify-center items-center gap-2 cursor-pointer">
             <div><i className="text-green-400 fa-solid fa-circle-user fa-xl"></i></div>
-            <div className="relative">UserName
+            <div className="relative border-none bg-transparent select-none bg-gray-900 rounded-[48px] text-base font-light font-['Inter'] leading-7">UserName
               
               <div className="absolute hidden mt-1 bg-slate-500 p-2 right-[-20px] w-[6rem] rounded" id="userDropdown"><ul>
                 <li className="">Sign Out</li>
