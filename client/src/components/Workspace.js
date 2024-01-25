0c8536facf8453e3c9f10ae272742c9dd21dd7f8
Harsh Vardhan Gupta <harsh247@gmail.com>
2024-01-26 00:05:02 +0530

    made changes to style of workspace page

--- client/src/components/Workspace.js
+++ client/src/components/Workspace.js
@@ -9,6 +9,7 @@ const Workspace = () => {
   function handleUserDropdown(){
     const elem = document.getElementById("userDropdown");
     elem.classList.toggle("hidden");
+    document.getElementById("dropDownArrow").classList.toggle("rotate-180");
   }
 
 
@@ -27,18 +28,18 @@ const Workspace = () => {
           <button className="mr-10 text-xl p-1.5  rounded-[48px] text-yellow-600 pl-[25px] pr-[25.05px] py-[11px] hidden sm:inline">PREMIUM</button>
           <span onClick={handleUserDropdown} className="flex justify-center items-center gap-2 cursor-pointer">
             <div><i className="text-green-400 fa-solid fa-circle-user fa-xl"></i></div>
-            <div className="relative border-none bg-transparent select-none bg-gray-900 rounded-[48px] text-base font-light font-['Inter'] leading-7">UserName
+            <div className="relative border-none bg-transparent select-none  rounded-[48px] text-base font-light font-['Inter'] leading-7 ">UserName
               
-              <div className="absolute hidden mt-1 bg-[#0A2647] p-1 right-[-20px] w-[7rem] rounded z-20 gap-1" id="userDropdown"><ul>
-              <li className="border-b-[.1rem] p-1 hover:bg-[#07192f] transition-all duration-200">Profile</li>
-              <li className="border-b-[.1rem] p-1 hover:bg-[#07192f] transition-all duration-200">Subscribe</li>
-              <li className=" p-1 hover:bg-[#07192f] transition-all duration-200">Sign Out</li>
+              <div className="absolute transition-all duration-200 hidden mt-1 bg-[#0A2647] p-1 right-[-20px] w-[6rem] rounded z-20 gap-1" id="userDropdown"><ul>
+              <a href="/"><li className="border-b-[.1rem] p-1 hover:bg-[#07192f] transition-all duration-200">Profile</li></a>
+              <a href="/"><li className="border-b-[.1rem] p-1 hover:bg-[#07192f] transition-all duration-200">Plans</li></a>
+              <a href="/"><li className=" p-1 hover:bg-[#07192f] transition-all duration-200">Sign Out</li></a>
               
                 
               </ul></div>
               </div>
 
-            <div><i class="fa-solid fa-caret-down"></i></div>
+            <div id ="dropDownArrow" className="transition-all duration-200 origin-center"><i class="fa-solid fa-caret-down"></i></div>
 
           </span>
         </div>
